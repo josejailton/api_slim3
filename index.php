18c884a113e2e07a5983817143e600ea7f9e526f
William <wilcorrea@gmail.com>
2016-11-15 23:11:11 -0200

    ...

--- index.php
+++ index.php
@@ -20,6 +20,13 @@ $app->get('/{version}/product/{id}', function (Request $request, Response $respo
     return $controller->read($parameters['id']);
 });
 
+$app->get('/{version}/product/{id}/{behaviour}', function (Request $request, Response $response, $parameters) use ($database) {
+
+    $controller = new ProductController($request, $response, $database);
+
+    return $controller->read($parameters['id'], $parameters['behaviour']);
+});
+
 $app->post('/{version}/product', function (Request $request, Response $response, $parameters) use ($database) {
 
     $controller = new ProductController($request, $response, $database);
--- private/app/src/Http/ProductController.php
+++ private/app/src/Http/ProductController.php
@@ -11,15 +11,32 @@ class ProductController extends AbstractController
 {
     /**
      * @param $id
+     * @param $behaviour
      * @return Response
      */
-    public function read($id = null)
+    public function read($id = null, $behaviour = null)
     {
-        //select products.id as id, brands.brand_name as brand from products INNER JOIN brands ON products.brand_id = brands.id
-        $data = $this->connection()
-            ->collection('products INNER JOIN brands ON (products.brand_id = brands.id)')
-            ->fields(['products.id as id', 'brands.brand_name as brand'])
-            ->where('products.id = ?')->read($id);
+        $connection = $this->connection();
+
+        $connection->collection('products');
+
+        switch ($behaviour) {
+            case 'brand': {
+                //select products.id as id, brands.brand_name as brand from products INNER JOIN brands ON products.brand_id = brands.id
+                $connection
+                    ->collection('products INNER JOIN brands ON (products.brand_id = brands.id)')
+                    ->fields(['products.id as id', 'brands.brand_name as brand']);
+                break;
+            }
+            case 'supplier': {
+                //select prod.id as id, supplier.supplier_name as supplier from products as prod INNER Join suppliers as supplier ON prod.supplier_id = supplier.id
+                $connection
+                    ->collection('products INNER JOIN suppliers ON (products.supplier_id = suppliers.id)')
+                    ->fields(['products.id as id', 'suppliers.supplier_name as supplier']);
+                break;
+            }
+        }
+        $data = $connection->where('products.id = ?')->read($id);
 
         if (!is_array($data)) {
             return $this->response($data, 500);
